a92d058175b1bdaae1ae2a75eb66cf21b84b6fef
JaumDark <viniciuspkfr@gmail.com>
2024-02-01 06:08:43 -0300

    feat: add first setup and auth

---
+++ app/(Landing)/page.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss'
+
+const LandingPage = () => {
+  return (
+    <div className={styles.container}>
+      Olá, seja bem vindo a sua Mão Direita
+    </div>
+  )
+}
+
+export default LandingPage
